fb93e60abfaadfe2572401625c4305a55747f7ea
Eric Guillemot <eguillemot79@gmail.com>
2021-12-06 23:32:09 +0100

    * try display again

--- librecoin/lc_display.py
+++ librecoin/lc_display.py
@@ -20,9 +20,9 @@ class lc_display:
 
     def draw(self):
         lines = ""
-        for x in range(0,self.m_screen_width):
+        for y in range(0,self.m_screen_height):    
             line = ""
-            for y in range(0,self.m_screen_height):    
+            for x in range(0,self.m_screen_width):
                 line += self.m_screen_datas[x][y]
             lines += line
         self.zero()
@@ -30,3 +30,12 @@ class lc_display:
 
     def zero(self):
         print("\033[%d;%dH" % (0, 0))
+
+    def print(self, text: str, x: int, y: int):
+        if x < 0 or x > self.m_screen_width:
+            return False
+        if y < 0 or y > self.m_screen_height:
+            return False
+        if text:
+            for i, c in enumerate(text):
+                self.m_screen_datas[x + i][y] = c

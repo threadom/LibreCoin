50b9bd95a29c5b5289fac5b854abf12ea15d8e7f
Eric Guillemot <eguillemot79@gmail.com>
2021-12-08 00:56:30 +0100

    * terminal interface ready

---
+++ views/config.py
@@ -0,0 +1,9 @@
+import librecoin
+
+def main(lc:librecoin):
+    lc.display().empty()
+    
+    lc.view().part("views.parts.top_menu")
+    lc.display().table(0,2,-0,-0)
+    lc.display().table(0,2,-0,3)
+    lc.display().ljust(" Config ", 1, 3, 0)

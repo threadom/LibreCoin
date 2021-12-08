8e8784a44cf0746e536f2f7212168418347a809a
Eric Guillemot <eguillemot79@gmail.com>
2021-12-08 19:41:00 +0100

    a

--- librecoin/lc_display.py
+++ librecoin/lc_display.py
@@ -1,5 +1,6 @@
 import os
 
+
 class lc_display:
     def __init__(self, librecoin: object):
         self.m_librecoin = librecoin
@@ -7,32 +8,56 @@ class lc_display:
         self.m_screen_width = librecoin.config().get('screen_width')
         self.m_screen_height = librecoin.config().get('screen_height')
         self.m_screen_datas = {}
+        self.m_screen_colors = {}
+        self.m_screen_bgcolors = {}
         self.m_current_view = False
         self.m_hourglass = False
         self.clear()
 
     def empty(self):
-        for x in range(0,self.m_screen_width):
+        for x in range(0, self.m_screen_width):
             self.m_screen_datas[x] = {}
-            for y in range(0,self.m_screen_height):    
-                self.m_screen_datas[x][y] = " "
+            self.m_screen_colors[x] = {}
+            self.m_screen_bgcolors[x] = {}
+            for y in range(0, self.m_screen_height):
+                self.m_screen_datas[x][y] = ' '
+                self.m_screen_colors[x][y] = False
+                self.m_screen_bgcolors[x][y] = False
 
     def clear(self):
+        # print('clear')
         os.system('cls' if os.name == 'nt' else 'clear')
 
     def draw(self):
-        lines = ""
-        for y in range(0,self.m_screen_height):    
-            line = ""
-            for x in range(0,self.m_screen_width):
+        old_color = False
+        lines = ''
+        for y in range(0, self.m_screen_height):
+            line = ''
+            for x in range(0, self.m_screen_width):
+                # Draw Color
+                color = ''
+                if self.m_screen_colors[x][y]:
+                    color = self.m_screen_colors[x][y]
+                bgcolor = ''
+                if self.m_screen_bgcolors[x][y]:
+                    bgcolor = self.m_screen_bgcolors[x][y]
+                new_color = self.getColorCode(color, bgcolor)
+                if not new_color:
+                    new_color = self.getNoColorCode()
+                if new_color != old_color:
+                    line += new_color
+                old_color = new_color
+                # Draw Text
                 line += self.m_screen_datas[x][y]
-            lines += line + "\r\n"
+            lines += line + '\r\n'
         lines = lines[:-2]
+        if old_color != self.getNoColorCode():
+            lines += self.getNoColorCode()
         self.zero()
-        print(lines, end = '')
+        print(lines, end='')
 
     def zero(self):
-        print("\033[%d;%dH" % (0, 0), end = '')
+        print('\033[%d;%dH' % (0, 0), end='')
 
     def print(self, text: str, x: int, y: int):
         if x < 0:
@@ -62,8 +87,8 @@ class lc_display:
         if y < 0 or y > self.m_screen_height:
             return False
         if text:
-            text = text.ljust(w)
-            # text = text.ljust(w,'.')
+            # text = text.ljust(w)
+            text = text.ljust(w+1)
             for i, c in enumerate(text):
                 self.m_screen_datas[x + i][y] = c
 
@@ -84,85 +109,105 @@ class lc_display:
         h = h - 1
 
         # CHARCODE ON https://en.wikipedia.org/wiki/Box-drawing_character
-        for i in range(x+1,x+w):
+        for i in range(x+1, x+w):
             # TOP HORIZONTAL LINE
-            if self.m_screen_datas[i][y] == chr(int("252C",16)):
-                self.m_screen_datas[i][y] = chr(int("252C",16))
-            elif self.m_screen_datas[i][y] == chr(int("2514",16)):
-                self.m_screen_datas[i][y] = chr(int("2534",16))
-            elif self.m_screen_datas[i][y] == chr(int("2518",16)):
-                self.m_screen_datas[i][y] = chr(int("2534",16))
-            elif self.m_screen_datas[i][y] == chr(int("2534",16)):
-                self.m_screen_datas[i][y] = chr(int("2534",16))
+            if self.m_screen_datas[i][y] == chr(int('252C', 16)):
+                self.m_screen_datas[i][y] = chr(int('252C', 16))
+            elif self.m_screen_datas[i][y] == chr(int('2514', 16)):
+                self.m_screen_datas[i][y] = chr(int('2534', 16))
+            elif self.m_screen_datas[i][y] == chr(int('2518', 16)):
+                self.m_screen_datas[i][y] = chr(int('2534', 16))
+            elif self.m_screen_datas[i][y] == chr(int('2534', 16)):
+                self.m_screen_datas[i][y] = chr(int('2534', 16))
             else:
-                self.m_screen_datas[i][y] = chr(int("2500",16))
+                self.m_screen_datas[i][y] = chr(int('2500', 16))
 
             # BOTTOM HORIZONTAL LINE
-            if self.m_screen_datas[i][y+h] == chr(int("2502",16)):
-                self.m_screen_datas[i][y+h] = chr(int("253C",16))
+            if self.m_screen_datas[i][y+h] == chr(int('2502', 16)):
+                self.m_screen_datas[i][y+h] = chr(int('253C', 16))
             else:
-                self.m_screen_datas[i][y+h] = chr(int("2500",16))
-        for i in range(y+1,y+h):
-            # VERTICAL LINE
-            if self.m_screen_datas[x][i] == chr(int("2524",16)):
-                self.m_screen_datas[x][i] = chr(int("2524",16))
+                self.m_screen_datas[i][y+h] = chr(int('2500', 16))
+        for i in range(y+1, y+h):
+            # LEFT VERTICAL LINE
+            if self.m_screen_datas[x][i] == chr(int('2524', 16)):
+                self.m_screen_datas[x][i] = chr(int('2524', 16))
             else:
-                self.m_screen_datas[x][i] = chr(int("2502",16))
-            self.m_screen_datas[x+w][i] = chr(int("2502",16))
+                self.m_screen_datas[x][i] = chr(int('2502', 16))
+            # RIGHT VERTICAL LINE
+            if self.m_screen_datas[x+w][i] == chr(int('2514', 16)):
+                self.m_screen_datas[x+w][i] = chr(int('251C', 16))
+            elif self.m_screen_datas[x+w][i] == chr(int('251C', 16)):
+                self.m_screen_datas[x+w][i] = chr(int('251C', 16))
+            else:
+                self.m_screen_datas[x+w][i] = chr(int('2502', 16))
 
         # TOP LEFT CORNER
-        if self.m_screen_datas[x][y] == chr(int("2500",16)):
-            self.m_screen_datas[x][y] = chr(int("252C",16))
-        elif self.m_screen_datas[x][y] == chr(int("253C",16)):
-            self.m_screen_datas[x][y] = chr(int("253C",16))
-        elif self.m_screen_datas[x][y] == chr(int("252C",16)):
-            self.m_screen_datas[x][y] = chr(int("252C",16))
-        elif self.m_screen_datas[x][y] == chr(int("251C",16)):
-            self.m_screen_datas[x][y] = chr(int("251C",16))
-        elif self.m_screen_datas[x][y] == chr(int("2520",16)):
-            self.m_screen_datas[x][y] = chr(int("2520",16))
-        elif self.m_screen_datas[x][y] == chr(int("2514",16)):
-            self.m_screen_datas[x][y] = chr(int("251C",16))
-        elif self.m_screen_datas[x][y] == chr(int("2510",16)):
-            self.m_screen_datas[x][y] = chr(int("252C",16))
+        if self.m_screen_datas[x][y] == chr(int('2500', 16)):
+            self.m_screen_datas[x][y] = chr(int('252C', 16))
+        elif self.m_screen_datas[x][y] == chr(int('253C', 16)):
+            self.m_screen_datas[x][y] = chr(int('253C', 16))
+        elif self.m_screen_datas[x][y] == chr(int('252C', 16)):
+            self.m_screen_datas[x][y] = chr(int('252C', 16))
+        elif self.m_screen_datas[x][y] == chr(int('251C', 16)):
+            self.m_screen_datas[x][y] = chr(int('251C', 16))
+        elif self.m_screen_datas[x][y] == chr(int('2520', 16)):
+            self.m_screen_datas[x][y] = chr(int('2520', 16))
+        elif self.m_screen_datas[x][y] == chr(int('2514', 16)):
+            self.m_screen_datas[x][y] = chr(int('251C', 16))
+        elif self.m_screen_datas[x][y] == chr(int('2510', 16)):
+            self.m_screen_datas[x][y] = chr(int('252C', 16))
         else:
-            self.m_screen_datas[x][y] = chr(int("250C",16))
+            self.m_screen_datas[x][y] = chr(int('250C', 16))
 
-        # TOP RIGHT CORNER  
-        if self.m_screen_datas[x+w][y] == chr(int("252C",16)):
-            self.m_screen_datas[x+w][y] = chr(int("252C",16))
-        elif self.m_screen_datas[x+w][y] == chr(int("2500",16)):
-            self.m_screen_datas[x+w][y] = chr(int("252C",16))
-        elif self.m_screen_datas[x+w][y] == chr(int("2524",16)):
-            self.m_screen_datas[x+w][y] = chr(int("2524",16))
-        elif self.m_screen_datas[x+w][y] == chr(int("2518",16)):
-            self.m_screen_datas[x+w][y] = chr(int("2524",16))
-        elif self.m_screen_datas[x+w][y] == chr(int("2534",16)):
-            self.m_screen_datas[x+w][y] = chr(int("253C",16))
-        elif self.m_screen_datas[x+w][y] == chr(int("250C",16)):
-            self.m_screen_datas[x+w][y] = chr(int("252C",16))    
+        # TOP RIGHT CORNER
+        if self.m_screen_datas[x+w][y] == chr(int('253C', 16)):
+            self.m_screen_datas[x+w][y] = chr(int('253C', 16))
+        elif self.m_screen_datas[x+w][y] == chr(int('2502', 16)):
+            self.m_screen_datas[x+w][y] = chr(int('2524', 16))
+        elif self.m_screen_datas[x+w][y] == chr(int('252C', 16)):
+            self.m_screen_datas[x+w][y] = chr(int('252C', 16))
+        elif self.m_screen_datas[x+w][y] == chr(int('2500', 16)):
+            self.m_screen_datas[x+w][y] = chr(int('252C', 16))
+        elif self.m_screen_datas[x+w][y] == chr(int('2524', 16)):
+            self.m_screen_datas[x+w][y] = chr(int('2524', 16))
+        elif self.m_screen_datas[x+w][y] == chr(int('2518', 16)):
+            self.m_screen_datas[x+w][y] = chr(int('2524', 16))
+        elif self.m_screen_datas[x+w][y] == chr(int('2534', 16)):
+            self.m_screen_datas[x+w][y] = chr(int('253C', 16))
+        elif self.m_screen_datas[x+w][y] == chr(int('250C', 16)):
+            self.m_screen_datas[x+w][y] = chr(int('252C', 16))
         else:
-            self.m_screen_datas[x+w][y] = chr(int("2510",16))
+            self.m_screen_datas[x+w][y] = chr(int('2510', 16))
 
         # BOTTOM LEFT CORNER
-        if self.m_screen_datas[x][y+h] == chr(int("2500",16)):
-            self.m_screen_datas[x][y+h] = chr(int("2534",16))    
-        elif self.m_screen_datas[x][y+h] == chr(int("2524",16)):
-            self.m_screen_datas[x][y+h] = chr(int("253C",16))    
-        elif self.m_screen_datas[x][y+h] == chr(int("2502",16)):
-            self.m_screen_datas[x][y+h] = chr(int("251C",16))    
-        elif self.m_screen_datas[x][y+h] == chr(int("2518",16)):
-            self.m_screen_datas[x][y+h] = chr(int("2534",16))    
+        if self.m_screen_datas[x][y+h] == chr(int('2534', 16)):
+            self.m_screen_datas[x][y+h] = chr(int('2534', 16))
+        elif self.m_screen_datas[x][y+h] == chr(int('2500', 16)):
+            self.m_screen_datas[x][y+h] = chr(int('2534', 16))
+        elif self.m_screen_datas[x][y+h] == chr(int('2524', 16)):
+            self.m_screen_datas[x][y+h] = chr(int('253C', 16))
+        elif self.m_screen_datas[x][y+h] == chr(int('2502', 16)):
+            self.m_screen_datas[x][y+h] = chr(int('251C', 16))
+        elif self.m_screen_datas[x][y+h] == chr(int('2518', 16)):
+            self.m_screen_datas[x][y+h] = chr(int('2534', 16))
         else:
-            self.m_screen_datas[x][y+h] = chr(int("2514",16))
+            self.m_screen_datas[x][y+h] = chr(int('2514', 16))
 
         # BOTTOM RIGHT CORNER
-        if self.m_screen_datas[x+w][y+h] == chr(int("2502",16)):
-            self.m_screen_datas[x+w][y+h] = chr(int("2524",16))    
-        elif self.m_screen_datas[x+w][y+h] == chr(int("2514",16)):
-            self.m_screen_datas[x+w][y+h] = chr(int("2534",16))    
+        if self.m_screen_datas[x+w][y+h] == chr(int('2534', 16)):
+            self.m_screen_datas[x+w][y+h] = chr(int('2534', 16))
+        elif self.m_screen_datas[x+w][y+h] == chr(int('251C', 16)):
+            self.m_screen_datas[x+w][y+h] = chr(int('253C', 16))
+        elif self.m_screen_datas[x+w][y+h] == chr(int('2500', 16)):
+            self.m_screen_datas[x+w][y+h] = chr(int('2534', 16))
+        elif self.m_screen_datas[x+w][y+h] == chr(int('2524', 16)):
+            self.m_screen_datas[x+w][y+h] = chr(int('2524', 16))
+        elif self.m_screen_datas[x+w][y+h] == chr(int('2502', 16)):
+            self.m_screen_datas[x+w][y+h] = chr(int('2524', 16))
+        elif self.m_screen_datas[x+w][y+h] == chr(int('2514', 16)):
+            self.m_screen_datas[x+w][y+h] = chr(int('2534', 16))
         else:
-            self.m_screen_datas[x+w][y+h] = chr(int("2518",16))
+            self.m_screen_datas[x+w][y+h] = chr(int('2518', 16))
 
     def hourglass(self, x: int, y: int):
         if x < 0:
@@ -170,13 +215,97 @@ class lc_display:
         if y < 0:
             y = (self.m_screen_height + y)
 
-        if self.m_hourglass == chr(int("2557",16)):
-            self.m_hourglass = chr(int("255D",16))
-        elif self.m_hourglass == chr(int("2554",16)):
-            self.m_hourglass = chr(int("2557",16))
-        elif self.m_hourglass == chr(int("255A",16)):
-            self.m_hourglass = chr(int("2554",16))
+        if self.m_hourglass == chr(int('2557', 16)):
+            self.m_hourglass = chr(int('255D', 16))
+        elif self.m_hourglass == chr(int('2554', 16)):
+            self.m_hourglass = chr(int('2557', 16))
+        elif self.m_hourglass == chr(int('255A', 16)):
+            self.m_hourglass = chr(int('2554', 16))
         else:
-            self.m_hourglass = chr(int("255A",16))
-        
+            self.m_hourglass = chr(int('255A', 16))
+
         self.m_screen_datas[x][y] = self.m_hourglass
+
+    def color(self, color: str, x: int, y: int, w: int, h: int):
+        if x < 0:
+            x = (self.m_screen_width + x)
+        if y < 0:
+            y = (self.m_screen_height + y)
+        if w == 0:
+            w = self.m_screen_width - x
+        if h == 0:
+            h = self.m_screen_height - y
+        if w < 0:
+            w = self.m_screen_width - x + w
+        if h < 0:
+            h = self.m_screen_height - x + h
+        # print(x)
+        # print(y)
+        # print(w)
+        # print(h)
+        for j in range(y, y+h):
+            # print('j'+str(j))
+            for i in range(x, x+w):
+                # print('i'+str(i))
+                self.m_screen_colors[i][j] = color
+        # print(self.m_screen_colors)
+
+    def bgcolor(self, bgcolors: str, x: int, y: int, w: int, h: int):
+        if x < 0:
+            x = (self.m_screen_width + x)
+        if y < 0:
+            y = (self.m_screen_height + y)
+        if w == 0:
+            w = self.m_screen_width - x
+        if h == 0:
+            h = self.m_screen_height - y
+        if w < 0:
+            w = self.m_screen_width - x + w
+        if h < 0:
+            h = self.m_screen_height - x + h
+        for j in range(y, y+h):
+            for i in range(x, x+w):
+                self.m_screen_bgcolors[i][j] = bgcolors
+
+    def getNoColorCode(self):
+        return '\033[00m'
+
+    def getColorCode(self, color: str, bgcolor: str):
+        colorcode = ''
+        match color:
+            case 'red':
+                colorcode += '\033[91m'
+            case 'green':
+                colorcode += '\033[92m'
+            case 'yellow':
+                colorcode += '\033[93m'
+            case 'blue':
+                colorcode += '\033[94m'
+            case 'purple':
+                colorcode += '\033[95m'
+            case 'cyan':
+                colorcode += '\033[96m'
+            case 'white':
+                colorcode += '\033[97m'
+            case 'gray':
+                colorcode += '\033[98m'
+        match bgcolor:
+            case 'black':
+                colorcode += '\033[40m'
+            case 'red':
+                colorcode += '\033[41m'
+            case 'green':
+                colorcode += '\033[42m'
+            case 'yellow':
+                colorcode += '\033[43m'
+            case 'blue':
+                colorcode += '\033[44m'
+            case 'purple':
+                colorcode += '\033[45m'
+            case 'cyan':
+                colorcode += '\033[46m'
+            case 'gray':
+                colorcode += '\033[47m'
+        if color or bgcolor:
+            return colorcode
+        return False
--- views/dashboard.py
+++ views/dashboard.py
@@ -8,18 +8,46 @@ def init(lc:librecoin):
     lc.view().part("views.parts.top_menu").init()
     lc.display().table(0,2,-0,-0)
     lc.display().table(0,2,-0,3)
-    lc.display().ljust("Dashboard", 2, 3, 0)
+    lc.display().ljust("Dashboard", 2, 3, -1)
 
     lc.display().table(0,4,-0,15)
     lc.display().table(0,4,-29*7,3)
-    lc.display().ljust("Wallet History Chart", 2, 5, -29*7)
+    lc.display().ljust("Wallet History Chart", 2, 5, -29*7-1)
+    lc.display().color('red', 2, 5, -29*7-1, 1)
+    
+    # lc.display().ljust("Wallet History Chart", 2, 7, -29*7-1)
+    # lc.display().ljust("Wallet History Chart", 2, 8, -29*7-1)
+    # lc.display().ljust("Wallet History Chart", 2, 9, -29*7-1)
+    # lc.display().ljust("Wallet History Chart", 2, 10, -29*7-1)
+    # lc.display().ljust("Wallet History Chart", 2, 11, -29*7-1)
+    # lc.display().ljust("Wallet History Chart", 2, 12, -29*7-1)
+    # lc.display().ljust("Wallet History Chart", 2, 13, -29*7-1)
+    # lc.display().ljust("Wallet History Chart", 2, 14, -29*7-1)
 
+    # lc.display().color('red', 2, 7, -29*7-1, 1)
+    # lc.display().color('green', 2, 8, -29*7-1, 1)
+    # lc.display().color('yellow', 2, 9, -29*7-1, 1)
+    # lc.display().color('blue', 2, 10, -29*7-1, 1)
+    # lc.display().color('purple', 2, 11, -29*7-1, 1)
+    # lc.display().color('cyan', 2, 12, -29*7-1, 1)
+    # lc.display().color('white', 2, 13, -29*7-1, 1)
+    # lc.display().color('gray', 2, 14, -29*7-1, 1)
+
+    # lc.display().bgcolor('red', -29*7, 7, -29*6-1, 1)
+    # lc.display().bgcolor('green', -29*7, 8, -29*6-1, 1)
+    # lc.display().bgcolor('yellow', -29*7, 9, -29*6-1, 1)
+    # lc.display().bgcolor('blue', -29*7, 10, -29*6-1, 1)
+    # lc.display().bgcolor('purple', -29*7, 11, -29*6-1, 1)
+    # lc.display().bgcolor('cyan', -29*7, 12, -29*6-1, 1)
+    # lc.display().bgcolor('gray', -29*7, 13, -29*6-1, 1)
+    # lc.display().bgcolor('black', -29*7, 14, -29*6-1, 1)
+    
     # Top5
     def Top5(title:str,x:int,y:int):
         lc.display().table(x-30,y,30,9)
         lc.display().table(x-30,y,30,3)
         lc.display().table(x-11,y+2,11,7)
-        lc.display().ljust(title, x-28, y+1, 28)
+        lc.display().ljust(title, x-28, y+1, 27)
 
     Top5("Top 5 Increase - Yearly", -29*6, 18)
     Top5("Top 5 Decrease - Yearly", -29*6, 26)
@@ -42,13 +70,13 @@ def init(lc:librecoin):
     lc.display().table(0, 20, -29*7, 3)
     lc.display().table(-29*7 -9, 22, 7, 0)
     lc.display().table(-29*7 -3, 22, 3, 0)
-    lc.display().ljust("WE:Wallet Evolution", 2, 19, -29*7)
-    lc.display().ljust("Minute Compare", 2, 21, -29*7)
+    lc.display().ljust("WE:Wallet Evolution", 2, 19, -29*7-1)
+    lc.display().ljust("Minute Compare", 2, 21, -29*7-1)
 
 def update(lc:librecoin):
     lc.view().part("views.parts.top_menu").update()
 
-    lc.display().print(str(random.randint(0,99)), 10, 10)
+    # lc.display().print(str(random.randint(0,99)), 10, 10)
 
 def control(lc:librecoin):
     lc.view().part("views.parts.top_menu").control()
